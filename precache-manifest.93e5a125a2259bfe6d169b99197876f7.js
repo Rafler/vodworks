1814348f7e0eb75cfe9e3894b9a01b6f6db95144
Rafler <amuronyk@gmail.com>
2020-02-20 14:22:32 +0200

    Updates

---
+++ precache-manifest.93e5a125a2259bfe6d169b99197876f7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0506ea2d7eb3d9780097174e54c4d6ed",
+    "url": "/vodworks/index.html"
+  },
+  {
+    "revision": "4e09de4d213da26b9d6d",
+    "url": "/vodworks/static/css/main.dd7a061c.chunk.css"
+  },
+  {
+    "revision": "2b47d906790c337a5c1f",
+    "url": "/vodworks/static/js/2.de95529d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/vodworks/static/js/2.de95529d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e09de4d213da26b9d6d",
+    "url": "/vodworks/static/js/main.299b0daa.chunk.js"
+  },
+  {
+    "revision": "034a3b3378c0e77389ae",
+    "url": "/vodworks/static/js/runtime-main.abda0e66.js"
+  },
+  {
+    "revision": "408bb5552613d9e6abcbfa93c2d1fc3c",
+    "url": "/vodworks/static/media/add.408bb555.svg"
+  },
+  {
+    "revision": "576b3fa71da8a2ee4080a46131eefbc2",
+    "url": "/vodworks/static/media/arrow.576b3fa7.svg"
+  },
+  {
+    "revision": "98ea0c65183bc7870de6202229cfb647",
+    "url": "/vodworks/static/media/eye.98ea0c65.svg"
+  },
+  {
+    "revision": "317ebf2b8ed3aad4615c4ebda3e8950c",
+    "url": "/vodworks/static/media/play.317ebf2b.svg"
+  }
+]);
